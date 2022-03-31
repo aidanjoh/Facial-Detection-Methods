23eb21396a28c76a9abd257bc8c51f5e11831ebc
Aidan Johnson <aidan.johnson1@ucalgary.ca>
2022-03-31 13:34:33 -0600

    Added non-cropped black and white images we can use and got the ability to show them with matplotlib. Will need to add bounding boxes to the images that we are interested in.

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
 import sys
 import cv2
+import dlib
+import skimage
+
+import matplotlib.pyplot as plt
+
 
 
 def main():
-    cascPath = "haarcascade_frontalface_default.xml"
-    newCascPath = "lbpcascade_frontalface_improved.xml"
-    faceCascade = cv2.CascadeClassifier(newCascPath)
+    with open("Images/yaleB11_P00A+000E+20.pgm", 'rb') as pgmf:
+        im = plt.imread(pgmf)
+        plt.imshow(im,cmap='gray', vmin=0, vmax=255)
+        plt.show()
 
-    video_capture = cv2.VideoCapture(0)
+    with open("Images/yaleB11_P00A+000E+45.pgm", 'rb') as pgmf:
+        im = plt.imread(pgmf)
+        plt.imshow(im, cmap='gray', vmin=0, vmax=255)
+        plt.show()
 
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
+    with open("Images/yaleB11_P00A+000E+90.pgm", 'rb') as pgmf:
+        im = plt.imread(pgmf)
+        plt.imshow(im, cmap='gray', vmin=0, vmax=255)
+        plt.show()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cascPath = "haarcascade_frontalface_default.xml"
+    # newCascPath = "lbpcascade_frontalface_improved.xml"
+    # faceCascade = cv2.CascadeClassifier(newCascPath)
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+    # video_capture = cv2.VideoCapture(0)
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # while True:
+    #     # Capture frame-by-frame
+    #     ret, frame = video_capture.read()
 
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
+    #     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    #     faces = faceCascade.detectMultiScale(
+    #         gray,
+    #         scaleFactor=1.1,
+    #         minNeighbors=5,
+    #         minSize=(30, 30),
+    #         flags=cv2.CASCADE_SCALE_IMAGE
+    #     )
 
-    # When everything is done, release the capture
-    video_capture.release()
-    cv2.destroyAllWindows()
+    #     # Draw a rectangle around the faces
+    #     for (x, y, w, h) in faces:
+    #         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    #     # Display the resulting frame
+    #     cv2.imshow('Video', frame)
+
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+
+    # # When everything is done, release the capture
+    # video_capture.release()
+    # cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
