03e4126f7a045bb8bde806fde944803c2de9bc8c
Cameron Faith <cmrnfaith@gmail.com>
2022-03-31 18:24:11 -0600

    abilty to draw points created. need to do grayscale

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ for i in range(len(df.index)):
   with open("Images/personal/"+ df["Name"][i], 'rb') as pgmf:
     im = plt.imread(pgmf)
     plt.imshow(im)
-    plt.plot([df["x1"][i],df["x2"][i],df["x4"][i],df["x3"][i]],[df["y1"][i],df["y2"][i],df["y4"][i],df["y3"][i]], 'y')
+    plt.plot([df["x1"][i],df["x2"][i],df["x4"][i],df["x3"][i],df["x1"][i]],[df["y1"][i],df["y2"][i],df["y4"][i],df["y3"][i],df["y1"][i]], 'y')
     plt.show()
