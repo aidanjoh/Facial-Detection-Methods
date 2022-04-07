7f0ca7d0508a6b7cfe5cfd54580a11be7ad27e56
Aidan Johnson <aidan.johnson1@ucalgary.ca>
2022-04-07 09:52:23 -0600

    Added more personal images.

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    with open("Images/YaleDB/yaleB11_P00A+000E+00.pgm", 'rb') as pgmf:
+    with open("Images/YaleDB/yaleB11_P01A+000E+00.pgm", 'rb') as pgmf:
         im = plt.imread(pgmf)
         plt.imshow(im,cmap='gray', vmin=0, vmax=255)
         plt.show()
 
-    with open("Images/YaleDB/yaleB11_P00A+000E+20.pgm", 'rb') as pgmf:
+    with open("Images/YaleDB/yaleB11_P03A+000E+20.pgm", 'rb') as pgmf:
         im = plt.imread(pgmf)
         plt.imshow(im, cmap='gray', vmin=0, vmax=255)
         plt.show()
