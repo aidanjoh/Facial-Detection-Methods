eae988a375e406c3d73a2017f73359c693d8d9b2
Aidan Johnson <aidan.johnson1@ucalgary.ca>
2022-04-07 17:42:06 -0600

    Went through all the images with the Haars cascade.

--- main.py
+++ main.py
@@ -41,6 +41,21 @@ def main():
     cascPath = "haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(cascPath)
 
+    for imageNum in range(1,11):
+        imagePath = f"Images/personal/{imageNum}.jpg"
+        grayImage = convertImageToGrayScale(imagePath)
+        viewImage(grayImage)
+        faces = haarsFaceDetect(faceCascade, grayImage, minSize=(30,30), scaleFactor=1.1, minNeighbors=5)
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(grayImage, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Display the resulting picture with the detected bounding box(es)
+        cv2.imshow('Face Detection', grayImage)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
     imagePath = "Images/personal/10.jpg"
     grayImage = convertImageToGrayScale(imagePath)
     viewImage(grayImage)
