cfb8ecd42068e409d42df28c2a66074e388f56c0
Cameron Faith <cmrnfaith@gmail.com>
2022-04-07 17:56:17 -0600

    Setup rough patch

--- setup.py
+++ setup.py
@@ -7,8 +7,13 @@ print (df.head)
 print (df['Name'][0] +"string")
 
 for i in range(len(df.index)):
+  if(i != 0 and df["Name"][i] == df["Name"][i-1]):
+    continue
   with open("Images/personal/"+ df["Name"][i], 'rb') as pgmf:
     im = plt.imread(pgmf)
     plt.imshow(im)
     plt.plot([df["x1"][i],df["x2"][i],df["x4"][i],df["x3"][i],df["x1"][i]],[df["y1"][i],df["y2"][i],df["y4"][i],df["y3"][i],df["y1"][i]], 'y')
+    if(i < (len(df.index)-1) and df["Name"][i] == df["Name"][i+1]):
+      i+=1
+      plt.plot([df["x1"][i],df["x2"][i],df["x4"][i],df["x3"][i],df["x1"][i]],[df["y1"][i],df["y2"][i],df["y4"][i],df["y3"][i],df["y1"][i]], 'y')
     plt.show()
