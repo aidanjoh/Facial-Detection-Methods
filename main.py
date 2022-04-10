f49dd072c41dd4f5bcaa91b1323fb0ee8c39bba9
Aidan Johnson <aidan.johnson1@ucalgary.ca>
2022-04-09 18:38:14 -0600

    Small changes.

--- main.py
+++ main.py
@@ -190,68 +190,69 @@ def main():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    # detector = dlib.get_frontal_face_detector()
-    # win = dlib.image_window()
+    detector = dlib.get_frontal_face_detector()
+    win = dlib.image_window()
 
-    # f = "Images/personal/1.jpg"
-    # img = dlib.load_rgb_image(f)
-    # detectedFaces = detector(img, 1)
-    # print("Number of faces detected: {}".format(len(detectedFaces)))
-    # for i, d in enumerate(detectedFaces):
-    #     print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(i, d.left (), d.top(), d.right (), d.bottom ()))
+    for imageNum in range(1,10):
+        imagePath = f"Images/personal/{imageNum}.jpg"
 
-    # win.clear_overlay()
-    # win.set_image(img)
-    # win.add_overlay(detectedFaces)
-    # dlib.hit_enter_to_continue()
+        img = dlib.load_rgb_image(imagePath)
+        detectedFaces = detector(img, 1)
+        print("Number of faces detected: {}".format(len(detectedFaces)))
 
-    # Finally, if you really want to you can ask the detector to tell you the score
-    # for each detection.  The score is bigger for more confident detections.
-    # The third argument to run is an optional adjustment to the detection threshold,
-    # where a negative value will return more detections and a positive value fewer.
-    # Also, the idx tells you which of the face sub-detectors matched.  This can be
-    # used to broadly identify faces in different orientations.
-    # if (len(f) > 0):
-    #     img = dlib.load_rgb_image(f)
-    #     dets, scores, idx = detector.run(img, 1, -1)
-    #     for i, d in enumerate(dets):
-    #         print("Detection {}, score: {}, face_type:{}".format(d, scores[i], idx[i]))
+        for i, d in enumerate(detectedFaces):
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(i, d.left(), d.top(), d.right(), d.bottom()))
+            predictedBox = [d.left(), d.top(), d.right(), d.bottom()]
+            if len(groundTrueBoundingBox[imageNum]) == 1:
+                print(groundTrueBoundingBox[imageNum][0])
+                iouScore = calcIntersectiontionOverUnion(np.array(groundTrueBoundingBox[imageNum][0]), predictedBox)
+                print(f"The IoU Score is: {iouScore}")
+            else:
+                pass
+        
+        plt.imshow(img)
+        img_desc = plt.gca()
+        plt.set_cmap('gray')
+
+        for patch in faces:
+
+            img_desc.add_patch(
+                patches.Rectangle(
+                    (patch['c'], patch['r']),
+                    patch['width'],
+                    patch['height'],
+                    fill=False,
+                    color='r',
+                    linewidth=2
+                )
+            )
+
+        plt.show()
+
+        # Draw a rectangle around the faces
+        win.clear_overlay()
+        win.set_image(img)
+        print(detectedFaces)
+        win.add_overlay(detectedFaces)
+        win.add_overlay([groundTrueBoundingBox[imageNum][0][0], groundTrueBoundingBox[imageNum][0][1],
+                        groundTrueBoundingBox[imageNum][0][2], groundTrueBoundingBox[imageNum][0][3]])
+        dlib.hit_enter_to_continue()
+
+        # Finally, if you really want to you can ask the detector to tell you the score
+        # for each detection.  The score is bigger for more confident detections.
+        # The third argument to run is an optional adjustment to the detection threshold,
+        # where a negative value will return more detections and a positive value fewer.
+        # Also, the idx tells you which of the face sub-detectors matched.  This can be
+        # used to broadly identify faces in different orientations.
+
+        dets, scores, idx = detector.run(img, 1, -1)
+        for i, d in enumerate(dets):
+            print("Detection {}, score: {}, face_type:{}".format(d, scores[i], idx[i]))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(grayImage, (x, y), (x+w, y+h), (0, 255, 0), 5)
 
 
-    # imagePath = "Images/personal/9.jpg"
-    # grayImage = convertImageToGrayScale(imagePath)
-    # #viewImage(grayImage)
-
-    # # Get all the faces from the image
-    # faces = haarsFaceDetect(faceCascade, grayImage, minSize=(30,30), scaleFactor=1.1, minNeighbors=5)
-   
-    # # faces = faceCascade.detectMultiScale(
-    # #                                         grayImage,
-    # #                                         scaleFactor=1.1,
-    # #                                         minNeighbors=5,
-    # #                                         minSize=(30, 30),
-    # #                                         flags=cv2.CASCADE_SCALE_IMAGE
-    # #                                     )
-
-    # # Draw a rectangle around the faces
-    # for (x, y, w, h) in faces:
-    #     cv2.rectangle(grayImage, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # # Display the resulting picture with the detected bounding box(es)
-    # cv2.imshow('Face Detection', grayImage)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    
-
-    # with open("Images/YaleDB/yaleB11_P01A+000E+00.pgm", 'rb') as pgmf:
-    #     im = plt.imread(pgmf)
-    #     plt.imshow(im,cmap='gray', vmin=0, vmax=255)
-    #     plt.show()
-
-    # with open("Images/YaleDB/yaleB11_P03A+000E+20.pgm", 'rb') as pgmf:
-    #     im = plt.imread(pgmf)
-    #     plt.imshow(im, cmap='gray', vmin=0, vmax=255)
-    #     plt.show()
 
 if __name__ == "__main__":
     main()
